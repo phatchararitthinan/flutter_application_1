f29b6cb4e9aa688ca90e7c1c780cca54c7849fb3
phatchararitthinan <phatchararitthinant66@nu.ac.th>
2025-07-29 10:06:50 +0700

    add container images

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,56 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  // จุดเริ่มต้นของแอป Flutter
+  runApp(const MyApp()); // บอก Flutter ว่าวิดเจ็ตใดคือรากฐานของแอป
 }
 
+// MyApp โดยทั่วไปคือวิดเจ็ตรากฐานของแอปพลิเคชันของคุณ
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key}); // Constructor
 
   @override
   Widget build(BuildContext context) {
-    //
+    // เมธอด build() ส่งคืนโครงสร้างต้นไม้วิดเจ็ตสำหรับวิดเจ็ตนี้
     return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
       title: 'Flutter Demo',
-      home: MyHomePage(
-        // โครงสร้างหน้าจอพื้นฐาน
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
       ),
-    );
+        // โครงสร้างหน้าจอพื้นฐาน
+        home: const MyHomePage(),
+      );
   }
 }
-
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       // ให้โครงสร้างแอปพื้นฐาน
       appBar: AppBar(
-        title: const Text('wellcom'),
-        backgroundColor: const Color.fromARGB(
-          255,
-          230,
-          83,
-          132,
-        ), // ปรับแต่งสี AppBar
+        title: const Text('Home Page'),
+        backgroundColor: Colors.pinkAccent, // ปรับแต่งสี AppBar
       ),
       body: Center(
+        // พื้นที่เนื้อหาหลัก
         child: Container(
           // Container นี้จะถูกจัดกึ่งกลาง
-          width: 300,
+          padding: EdgeInsets.all(10.0),
           height: 100,
           decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 114, 24, 54),
-            borderRadius: BorderRadius.all(Radius.circular(30.0)),
+            color: Colors.amber,
+            borderRadius: BorderRadius.horizontal(
+              left: Radius.circular(50.0),
+              right: Radius.circular(50.0),
+            ),
           ),
-          child: const Text('Constant Body Text'),
+          child: Center(
+          child: Image.asset('assets/download.jpg',
+          width: 100,
+          height: 50,
+          fit: BoxFit.cover,)
+         ),
         ),
       ),
 
